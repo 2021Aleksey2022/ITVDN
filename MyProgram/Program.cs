06dad0f96e5228389c2c58afc712509a85e7404b
Алексей <manay.ru@mail.ru>
2023-06-27 21:05:59 +1000

    Первый урок: Моя первая программа

---
+++ MyProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram
+{
+    class Program
+    {
+        /// <summary>
+        /// Первый Урок: Моя первая программа.
+        /// </summary>
+        /// <param name="Щербаков Алексей"></param>
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
